9ab1423ac62bc931a8ac112562a4aa1112e759fd
Ships <macbookpro@MacBook-Pro-de-Mac.local>
2018-08-15 00:47:47 +0200

    sixth_step

---
+++ solutions/step6.js
@@ -0,0 +1,14 @@
+//6th step ---
+
+const myModule = require('./myModule');
+const directory = process.argv[2];
+const extension = process.argv[3];
+myModule(directory, extension, (err, filteredList) => {
+    
+    if (err) return console.log(err);
+    
+    filteredList.forEach(file => {
+       
+        console.log(file);
+    });
+});
